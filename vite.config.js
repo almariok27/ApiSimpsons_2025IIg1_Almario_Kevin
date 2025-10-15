7883969f13d3a17d098c868712be4b0964e58d2b
AlmarioKevin <kevinalmario15@gmail.com>
2025-10-14 21:14:25 -0500

    ult

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ApiSimpsons_2025IIg1_Almario_Kevin_and_Lopez_Mauricio/',
+  base: '/ApiSimpsons_2025IIg1_Almario_Kevin/',
   server: {
-    host: true, 
-    port: 5173 
+    host: true,
+    port: 5173
   }
 })
